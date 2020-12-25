69cd551d6bae8cf2f4e87917d300d91c63e312e1
vishalpa8 <vishalpa8@gmail.com>
2020-12-25 10:11:41 +0530

    first project

---
+++ Color Flipper/button.js
@@ -0,0 +1,17 @@
+const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+const btn = document.getElementById("btn");
+
+const color = document.querySelector('.color');
+
+btn.addEventListener('click',function(){
+  let RandomNumber = "#";
+  for(let i=0; i<6; i++){
+      RandomNumber += hex[MyRandomPick()];
+  }
+  document.body.style.backgroundColor = RandomNumber;
+  color.textContent = RandomNumber;
+
+});
+function MyRandomPick(){
+  return Math.floor(Math.random()*hex.length);
+}
